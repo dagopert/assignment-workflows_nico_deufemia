11a03b9ea8e3d1e7a7eefd83f081c3c4d2b4afd8
Nicola Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2015-12-14 13:22:12 +0100

    refactor(extracted statement method): #determine amounts for each line was extracted to new method def amount_for(element)

--- 05-refactoring/customer.rb
+++ 05-refactoring/customer.rb
@@ -14,19 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = 0
-
-      # determine amounts for each line
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
+      this_amount = amount_for(element)
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -34,6 +22,7 @@ class Customer
       if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
         frequent_renter_points += 1
       end
+
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
       total_amount += this_amount
@@ -43,4 +32,19 @@ class Customer
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
+
+ def amount_for(element)
+     this_amount = 0
+     case element.movie.price_code
+     when Movie::REGULAR
+         this_amount += 2
+         this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+     when Movie::NEW_RELEASE
+         this_amount += element.days_rented * 3
+     when Movie::CHILDRENS
+         this_amount += 1.5
+         this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+     end
+ end
 end
+
